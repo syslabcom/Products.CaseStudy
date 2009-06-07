4bf2c7e9ab1af6a39c97b17d161097458f1c5f72
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2009-06-07 13:05:07 +0000

    new

---
+++ Products/CaseStudy/config.py
@@ -0,0 +1,8 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'Products.CaseStudy'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+}
---
+++ xliff.py
@@ -0,0 +1,9 @@
+from zope.interface import Interface
+try:
+    from slc.xliff.xliff import BaseAttributeExtractor
+except:
+    BaseAttributeExtractor = Interface
+
+class CaseStudyAttributeExtractor(BaseAttributeExtractor):
+    """ Adapter to retrieve attributes from a standard event based object """
+    attrs = ['title', 'description', 'text', 'action', 'results']
